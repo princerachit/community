60f855e2df87d4e91df18f8fd7eb841eee2a9051
princerachit <psinha36@gmail.com>
2018-07-05 21:16:38 +0530

    add read,delete,list scripts

--- feature-demos/cstor-cas-templates/crudops/demo_scripts/delete.sh
+++ feature-demos/cstor-cas-templates/crudops/demo_scripts/delete.sh
@@ -1,5 +1,1 @@
-#!bin/sh
-kubectl delete svc -l  "openebs.io/controller-service=cstor-controller-svc"
-kubectl delete CStorVolume --all
-kubectl delete CStorVolumeReplica --all
-kubectl delete deploy -l "openebs.io/controller=cstor-controller"
+curl -H "namespace: default" -X DELETE http://"$(kubectl get svc maya-apiserver-service --template={{.spec.clusterIP}})":5656/latest/volumes/cstor-cas-vol
