c3b02462d2863b65c73dd90aba9add37c50ef709
princerachit <psinha36@gmail.com>
2018-07-05 21:28:10 +0530

    add pool creation

--- feature-demos/cstor-cas-templates/crudops/demo_scripts/initial_setup.sh
+++ feature-demos/cstor-cas-templates/crudops/demo_scripts/initial_setup.sh
@@ -2,4 +2,5 @@
 kubectl apply -f ../openebs-operator.yaml
 kubectl apply -f ../crds
 kubectl apply -f ../cas_templates
-kubectl apply -f ../run-tasks.yaml 
+kubectl apply -f ../run-tasks.yaml
+kubectl apply -f ../cstor-pool.yaml
